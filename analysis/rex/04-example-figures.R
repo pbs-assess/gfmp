1067d28b9f25eb2636619f323725554bb494cc55
Sean Anderson <sean@seananderson.ca>
2020-12-16 13:47:55 -0800

    Translate SAR figs

--- analysis/rex/04-example-figures.R
+++ analysis/rex/04-example-figures.R
@@ -3,8 +3,19 @@ library("purrr")
 library("ggplot2")
 library("gfdlm")
 library("here")
+library("rosettafish")
 
-fig_dir <- here("report", "figure")
+FRENCH <- TRUE
+options(french = TRUE)
+
+if (!FRENCH) {
+  fig_dir <- here("report", "figure")
+} else {
+  fig_dir <- here("report-french", "figure")
+}
+
+dir.create("report", showWarnings = FALSE)
+dir.create("report-french", showWarnings = FALSE)
 if (!dir.exists(fig_dir)) dir.create(fig_dir)
 .ggsave <- function(filename, width, height, ...) {
   ggsave(file.path(fig_dir, paste0("framework-", filename, ".png")),
@@ -44,13 +55,25 @@ pm_df_list_all_rob <- readRDS(here("generated-data", "rex-pm-all-rob.rds"))
 
 mse2 <- readRDS(here("generated-data", "rex-mse-ind-ceq200.rds"))
 
-`LT LRP` <- gfdlm::pm_factory("SBMSY", 0.4, c(36, 50))
-`LT USR` <- gfdlm::pm_factory("SBMSY", 0.8, c(36, 50))
-FMSY <- DLMtool::PNOF
 STC <- gfdlm::pm_factory("LTY", 1, c(1, 10))
 LTC <- gfdlm::pm_factory("LTY", 1, c(36, 50))
 AADC <- gfdlm::pm_factory("AADC", ref_aadc, c(1, 50))
-PM <- c("LT LRP", "LT USR", "FMSY", "STC", "LTC", "AADC")
+
+if (!FRENCH) {
+  `LT LRP` <- gfdlm::pm_factory("SBMSY", 0.4, c(36, 50))
+  `LT USR` <- gfdlm::pm_factory("SBMSY", 0.8, c(36, 50))
+  FMSY <- DLMtool::PNOF
+} else {
+  `LT PRL` <- gfdlm::pm_factory("SBMSY", 0.4, c(36, 50))
+  `LT RSS` <- gfdlm::pm_factory("SBMSY", 0.8, c(36, 50))
+  FRMS <- DLMtool::PNOF
+}
+
+if (!FRENCH) {
+  PM <- c("LT LRP", "LT USR", "FMSY", "STC", "LTC", "AADC")
+} else {
+  PM <- c("LT PRL", "LT RSS", "FRMS", "STC", "LTC", "AADC")
+}
 
 mse2@OM$RefY <- ref_catch
 
@@ -61,6 +84,7 @@ mse@OM$RefY <- ref_catch
 pm_df <- gfdlm::get_probs(mse, PM)
 
 pm_df_list <- list("Scenario 1" = pm_df)
+if (FRENCH) names(pm_df_list)[1] <- paste(en2fr("Scenario", FRENCH), "1")
 
 catch_breaks <- seq(0, 600000, 100000)
 catch_labels <- catch_breaks / 1000
@@ -68,15 +92,15 @@ catch_labels <- catch_breaks / 1000
 # Convergence -----------------------------------------------------------------
 
 x3 <- mse
-x3@MPs <- paste0("MP-", seq_along(x3@MPs))
-gfdlm::plot_convergence(x3, c("LT USR")) +
+x3@MPs <- paste0(en2fr("MP", FRENCH), "-", seq_along(x3@MPs))
+gfdlm::plot_convergence(x3, PM[2]) +
   scale_colour_brewer(palette = "Dark2")
 .ggsave("convergence", 5, 3.5)
 
 # Main projections ------------------------------------------------------------
 
 x <- DLMtool::Sub(mse, MPs = c(".Itarget_d0.8"))
-x@MPs <- "MP-1"
+x@MPs <- x3@MPs[[1]]
 x %>% gfdlm::plot_main_projections(catch_breaks = catch_breaks, catch_labels = catch_labels)
 .ggsave("main-projections", 8, 2.5)
 
@@ -87,9 +111,11 @@ ind_breaks
 ind_labels <- ind_breaks / 100000
 ind_labels
 oddify <- function(x) seq(2, x, by = 2)
-DLMtool::Sub(mse, MPs = c(".Itarget_d0.8")) %>%
-  list("Scenario 1" = .) %>%
-  map(~{.x@MPs <- "MP 1"; .x}) %>%
+temp <- DLMtool::Sub(mse, MPs = c(".Itarget_d0.8")) %>%
+  list("Scenario 1" = .)
+if (FRENCH) names(temp)[1] <- paste(en2fr("Scenario", FRENCH), "1")
+temp %>%
+  map(~{.x@MPs <- paste0(en2fr("MP", FRENCH), " 1"); .x}) %>%
   gfdlm::plot_index(type = "AddInd", omit_index_fn = oddify,
     n_samples = 3, seed = 12345) + coord_cartesian(expand = FALSE, ylim = c(0, 10000000)) +
   scale_y_continuous(breaks = ind_breaks, labels = ind_labels)
@@ -99,60 +125,82 @@ DLMtool::Sub(mse, MPs = c(".Itarget_d0.8")) %>%
 
 mse2 %>%
   gfdlm::get_probs(PM) %>%
-  mutate(MP = c(paste0("MP-", seq_len(length(MP)-1)), "MP-ref")) %>%
+  mutate(MP = c(paste0(en2fr("MP", FRENCH), "-", seq_len(length(MP)-1)), paste0(en2fr("MP", FRENCH), "-ref"))) %>%
   gfdlm::plot_tigure(satisficed = c("LT LRP" = 0.9, "LTC" = 0.8))
 .ggsave("tigure", 3.5, 5.5)
 
 # Dot plot --------------------------------------------------------------------
 
-map(pm_df_list_all, dplyr::filter, MP %in% c("CC1.2", "CC1.0", "CC0.8", ".Itarget_x0.2", "FMSYref75")) %>%
-  map(~ {.x$MP <- c("MP-1", "MP-2", "MP-3", "MP-4", "MP-ref");.x}) %>%
-  gfdlm::plot_dots(type = "single") +
+.names <- c("MP-1", "MP-2", "MP-3", "MP-4", "MP-ref")
+if (FRENCH) .names <- gsub("MP", "PG", .names)
+
+temp <- map(pm_df_list_all, dplyr::filter, MP %in% c("CC1.2", "CC1.0", "CC0.8", ".Itarget_x0.2", "FMSYref75")) %>%
+  map(~ {.x$MP <- .names;.x}) %>%
+  map(~ {names(.x)[2:4] <- PM[1:3];.x})
+temp %>% gfdlm::plot_dots(type = "single") +
   scale_colour_manual(values = c( RColorBrewer::brewer.pal(4, "Dark2"), "grey50"))
 .ggsave("dot", 6, 3)
 
 # Trade-off plot --------------------------------------------------------------
 
-pm_df_list_all_rob$`no-cpue-light` %>%
+temp <- pm_df_list_all_rob$`no-cpue-light` %>%
   dplyr::filter(grepl("^.Itarg", MP)) %>%
-  list("Scenario 1" = .) %>%
-  map(~{.x$MP <- paste0("MP-", seq_along(.x$MP));.x}) %>%
-  gfdlm::plot_tradeoff("LT LRP", "STC") +
-  scale_colour_brewer(palette = "Dark2") +
-  coord_equal(xlim = c(0.6, 1), ylim = c(0.6, 1), expand = FALSE)+
-  guides(shape = "none")
+  list("Scenario 1" = .)
+if (FRENCH) names(temp)[1] <- paste(en2fr("Scenario", FRENCH), "1")
+
+if (!FRENCH) {
+  temp %>%
+    map(~{.x$MP <- paste0("MP-", seq_along(.x$MP));.x}) %>%
+    gfdlm::plot_tradeoff("LT LRP", "STC") +
+    scale_colour_brewer(palette = "Dark2") +
+    coord_equal(xlim = c(0.6, 1), ylim = c(0.6, 1), expand = FALSE)+
+    guides(shape = "none")
+} else {
+  colnames(temp[[1]])[2] <- "LT PRL"
+  temp %>%
+    map(~{.x$MP <- paste0("PG-", seq_along(.x$MP));.x}) %>%
+    gfdlm::plot_tradeoff("LT PRL", "STC", french = TRUE) +
+    scale_colour_brewer(palette = "Dark2") +
+    coord_equal(xlim = c(0.6, 1), ylim = c(0.6, 1), expand = FALSE)+
+    guides(shape = "none")
+}
 .ggsave("trade-off", 3.5, 4.5)
 
 # Radar plot ------------------------------------------------------------------
 
+.names <- c("MP-1", "MP-2", "MP-3", "MP-4")
+if (FRENCH) .names <- gsub("MP", "PG", .names)
 pm_df %>%
-  mutate(MP =  c("MP-1", "MP-2", "MP-3", "MP-4")) %>%
-  gfdlm::plot_radar(palette = "Dark2")
+  mutate(MP = .names) %>%
+  gfdlm::plot_radar(palette = "Dark2", french = FRENCH)
 .ggsave("radar", 5, 5)
 
 # Kobe ------------------------------------------------------------------------
 
 x <- DLMtool::Sub(mse, MPs = c(".Itarget_base"))
 x@MPs <- paste0("MP-", seq_along(x@MPs))
-gfdlm::plot_kobe_grid(list("Scenario 1" = x)) +
+if (FRENCH) x@MPs <- gsub("MP", "PG", x@MPs)
+temp <- list("Scenario 1" = x)
+if (FRENCH) names(temp)[1] <- paste(en2fr("Scenario", FRENCH), "1")
+gfdlm::plot_kobe_grid(temp, french = T) +
   coord_equal(xlim = c(0, 3.2), ylim = c(0, 2.1), expand = FALSE)
 .ggsave("kobe", 4.5, 2.7)
 
 # Worm plot -------------------------------------------------------------------
 
 # worm
-gfdlm::plot_worms_grid(list("Scenario 1" = x), include_historical = TRUE) +
+gfdlm::plot_worms_grid(temp, include_historical = TRUE) +
   coord_equal(xlim = c(0, 2.5), ylim = c(0, 1.5), expand = FALSE)
 .ggsave("worm", 4.5, 2.5)
 
 # Scenario projection sensitivities -------------------------------------------
 
-purrr::map(seq_along(mse_all), ~ DLMtool::Sub(mse_all[[.x]], MPs = c(".Itarget_d0.8"))) %>%
-  set_names(c("Scenario 1", "Scenario 2")) %>%
-  purrr::map(~ {.x@MPs <- "MP-1";.x}) %>%
+temp <- purrr::map(seq_along(mse_all), ~ DLMtool::Sub(mse_all[[.x]], MPs = c(".Itarget_d0.8"))) %>%
+  set_names(c("Scenario 1", "Scenario 2"))
+if (FRENCH) names(temp) <- c("Scénario 1", "Scénario 2")
+temp %>% purrr::map(~ {.x@MPs <- if (!FRENCH) "MP-1" else "PG-1";.x}) %>%
   gfdlm::plot_scenario_projections(catch_breaks = catch_breaks, catch_labels = catch_labels)
 .ggsave("sens-projections", 8, 2.5)
-
 
 # Substantially speeds up LaTeX rendering on a Mac
 # by pre-optimizing the PNG compression:
@@ -160,7 +208,7 @@ optimize_png <- TRUE
 if (optimize_png && !identical(.Platform$OS.type, "windows")) {
   files_per_core <- 1
   cores <- floor(parallel::detectCores() / 2L)
-  setwd("report/figure")
+  setwd(fig_dir)
   system(paste0(
     "find -X . -name 'framework-*.png' -print0 | xargs -0 -n ",
     files_per_core, " -P ", cores, " optipng -strip all"
